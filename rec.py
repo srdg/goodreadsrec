99dfd3d1fc0be6a5a4d820d8de53253bb8530adf
Soumik Ranjan Dasgupta <srd1908@cse.jgec.ac.in>
2019-11-22 13:45:13 +0530

    First commit

---
+++ rec.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+from goodreads import client
+
+gc = client.GoodreadsClient('ZfHUtSBSMU3ZOTo82zl7wA','OlI30Km37ikK1EIgG7jwWDxeiaAJDX10Fm9zw9XTUA')
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+	return render_template('form.html' )
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+	try:
+		text = request.form['text']
+		processed_text = int(text.strip())
+		links = gc.book(processed_text).similar_books[:7]
+		links.insert(0,gc.book(processed_text))
+		print(links)
+		return render_template('index.html', len = 7, links=links)
+	except:
+		return render_template('404.html')
+	
+if __name__=="__main__":
+	app.run(host="localhost")
+
+
