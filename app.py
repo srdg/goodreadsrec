6ff5d0801d3ef2f740918097fe96f666fc21a867
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-01-06 22:33:32 +0530

    Modularised code

--- app.py
+++ app.py
@@ -14,20 +14,30 @@ def form():
 @app.route('/', methods=['POST'])
 def form_post():
 	try:
-		user_input = request.form['text']
-		query_num = int(user_input.strip())
-		if request.form['userChoice'] == 'isbn_no':
-    			book_obj = gc.book(isbn=query_num)
-		else:
-    			book_obj = gc.book(query_num)
-		links = book_obj.similar_books[:7]
+		links = get_search_tag()
 		links.insert(0,gc.book(query_num))
 		print(book_obj)
 		return render_template('index.html', len = 7, links=links)
 	except:
 		return render_template('404.html')
 
-def main():
+def get_search_tag():
+	user_input = request.form['text']
+	query_num = int(user_input.strip())
+	if search_by_id() == true:
+		book_obj = gc.book(query_num)
+	else:
+		book_obj = gc.book(query_num)
+	links = book_obj.similar_books[:7]
+	return links
+
+def search_by_id():
+	if request.form['userChoice'] == 'isbn_no':
+    		return false
+	else:
+		return true
+
+	def main():
 	app.run()
 
 if __name__=="__main__":
