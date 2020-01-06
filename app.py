5cd725358ba6b933d5659a1e8244a71ef1f04156
SuvamPrasd <tituprasad30@gmail.com>
2020-01-06 14:27:13 +0530

    Updated app.py resolved issue #3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from goodreads import client
 import os
 
@@ -10,20 +10,25 @@ app = Flask(__name__)
 def form():
 	return render_template('form.html' )
 
+
 @app.route('/', methods=['POST'])
 def form_post():
 	try:
-		text = request.form['text']
-		processed_text = int(text.strip())
-		links = gc.book(processed_text).similar_books[:7]
-		links.insert(0,gc.book(processed_text))
-		print(links)
+		user_input = request.form['text']
+		query_num = int(user_input.strip())
+		if request.form.get('userChoice') == 'isbn_no':
+    			book_obj = gc.book(isbn=query_num)
+		else:
+    			book_obj = gc.book(query_num)
+		book_obj = book_obj.similar_books[:7]
+		book_obj.insert(0,gc.book(query_num))
+		print(book_obj)
 		return render_template('index.html', len = 7, links=links)
 	except:
 		return render_template('404.html')
 
 def main():
-	app.run()
+	app.run(debug=True)
 
 if __name__=="__main__":
 	main()
