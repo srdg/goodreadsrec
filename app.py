3964afc31b3220762ea5aad247cb6d16e43a1f64
Soumik Ranjan Dasgupta <dasguptaarjo@gmail.com>
2020-01-07 12:15:35 +0530

    Revert "Modularised the code in app.py"

--- app.py
+++ app.py
@@ -14,28 +14,18 @@ def form():
 @app.route('/', methods=['POST'])
 def form_post():
 	try:
-		links = get_search_tag()
+		user_input = request.form['text']
+		query_num = int(user_input.strip())
+		if request.form['userChoice'] == 'isbn_no':
+    			book_obj = gc.book(isbn=query_num)
+		else:
+    			book_obj = gc.book(query_num)
+		links = book_obj.similar_books[:7]
 		links.insert(0,gc.book(query_num))
+		print(book_obj)
 		return render_template('index.html', len = 7, links=links)
 	except:
 		return render_template('404.html')
-
-def get_search_tag():
-	user_input = request.form['text']
-	query_num = int(user_input.strip())
-	if search_by_id() == true:
-		book_obj = gc.book(query_num)
-	else:
-		book_obj = gc.book(isbn=query_num)
-	print(book_obj)
-	links = book_obj.similar_books[:7]
-	return links
-
-def search_by_id():
-	if request.form['userChoice'] == 'isbn_no':
-    		return False
-	else:
-		return True
 
 def main():
 	app.run()
