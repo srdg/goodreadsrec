698f6fcb8c8cd76f27e0c2927740c60154db4e08
Soumik <soumik@cse.jgec.ac.in>
2020-07-11 11:34:07 +0530

    add working version

--- app.py
+++ app.py
@@ -5,32 +5,34 @@ import os
 gc = client.GoodreadsClient(os.environ.get('GCLIENT_TOKEN'),os.environ.get('GCLIENT_KEY'))
 
 app = Flask(__name__)
+links = []
 
 @app.route('/')
 def form():
-	return render_template('form.html' )
+	return render_template('form.html')
 
 @app.route('/search/ID/<idnum>')
 def display_booksById(idnum):
-	links = request.args['links']
 	return render_template('index.html', len = 7, links=links)
 
 @app.route('/search/ISBN/<isbn>')
 def display_booksByIsbn(isbn):
-	links = request.args['links']
 	return render_template('index.html', len = 7, links=links)
 
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def form_post():
+	global links
+	links=[]
 	try:
 		links,book_obj,query_num = get_search_tag()
 		links.insert(0,book_obj)
+		print(type(links[0]))
 		if search_by_id() == True:
-			return redirect(url_for('display_booksById', idnum=query_num, len=7, links=links))
+			return redirect(url_for('display_booksById', idnum=query_num, len=7))
 		else:
-			return redirect(url_for('display_booksByIsbn', isbn=query_num, len=7, links=links))
+			return redirect(url_for('display_booksByIsbn', isbn=query_num, len=7))
 	except KeyError:
 		return render_template('notFound.html')
 	except IndexError:
@@ -39,6 +41,7 @@ def form_post():
 		return render_template('404.html')
 
 def get_search_tag():
+	global links
 	user_input = request.form['text']
 	query_num = user_input.strip()
 	if search_by_id() == True:
